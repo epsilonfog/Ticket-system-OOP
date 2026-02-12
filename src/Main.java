f2601d615104843725019f1ccd6ba66ae787a451
epsilonfog <epsilonfog@gmail.com>
2026-02-12 23:25:46 +0330

    Update Main.java to use TicketFactory

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,16 @@
 package edu.ticket;
 
-import edu.ticket.state.CreatedState;
-import edu.ticket.state.TicketState;
-import edu.ticket.strategy.ResponseStrategy;
+import edu.ticket.factory.TicketFactory;
+import edu.ticket.factory.TicketType;
 
-public class Ticket {
+public class Main {
 
-    private TicketState state;
-    private ResponseStrategy responseStrategy;
+    public static void main(String[] args) {
 
-    public Ticket(ResponseStrategy responseStrategy) {
-        this.state = new CreatedState();
-        this.responseStrategy = responseStrategy;
-    }
+        Ticket webTicket = TicketFactory.createTicket(TicketType.WEB);
+        Ticket emailTicket = TicketFactory.createTicket(TicketType.EMAIL);
 
-    public void process() {
-        state.handle(this);
-        responseStrategy.sendResponse(this);
-    }
-
-    public void setState(TicketState state) {
-        this.state = state;
-    }
-
-    public String getStateName() {
-        return state.getName();
+        webTicket.process();
+        emailTicket.process();
     }
 }
