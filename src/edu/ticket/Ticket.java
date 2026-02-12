7cfd96092e04b70a0be2301e45e2020a6d6b63bb
epsilonfog <epsilonfog@gmail.com>
2026-02-12 22:48:36 +0330

    Finalize Ticket with State, Strategy, and Logger

--- src/edu/ticket/Ticket.java
+++ src/edu/ticket/Ticket.java
@@ -1,23 +1,20 @@
-import edu.ticket.state.CreatedState;
+package edu.ticket;
+
 import edu.ticket.state.TicketState;
+import edu.ticket.state.CreatedState;
+import edu.ticket.strategy.ResponseStrategy;
 import edu.ticket.logger.SystemLogger;
 
 public class Ticket {
 
     private TicketState state;
+    private ResponseStrategy responseStrategy;
 
-    public Ticket() {
-        this.state = new CreatedState();
+    public Ticket(ResponseStrategy responseStrategy) {
+        this.state = new CreatedState(); 
+        this.responseStrategy = responseStrategy;
     }
 
-    public void process() {
-        state.handle(this);
-        state.handle(this);
-    responseStrategy.sendResponse(this);
-
-    SystemLogger.getInstance()
-        .log("Ticket processed. Current state: " + getStateName());
-    }
 
     public void setState(TicketState state) {
         this.state = state;
@@ -25,5 +22,20 @@ public class Ticket {
 
     public String getStateName() {
         return state.getName();
+    }
+
+   
+
+    public void setResponseStrategy(ResponseStrategy responseStrategy) {
+        this.responseStrategy = responseStrategy;
+    }
+
+
+    public void process() {
+        state.handle(this);
+        responseStrategy.sendResponse(this);
+
+        SystemLogger.getInstance()
+                .log("Ticket processed. Current state: " + getStateName());
     }
 }
